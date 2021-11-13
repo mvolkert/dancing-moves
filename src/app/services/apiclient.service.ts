3da2fb141b6e1a629d0cbe96ad79e9760d639497
Marco Volkert <marco.volkert24@gmx.de>
2021-11-13 14:58:02 +0100

    writing line sheets

--- src/app/services/apiclient.service.ts
+++ src/app/services/apiclient.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { MoveDto } from '../model/move-dto';
-import { parseBoolean, parseDate } from '../util/util';
+import { parseBoolean, parseDate, toGermanDate } from '../util/util';
 import { SettingsService } from './settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiclientService {
+  private keys: string[] = new Array<string>();
   constructor(private settingsService: SettingsService) { }
 
   private initClient(callback: () => void) {
@@ -17,7 +18,9 @@ export class ApiclientService {
     gapi.load('client:auth2', () => gapi.client.init({
       apiKey: this.settingsService.secret?.apiKey,
       discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(callback));
+    }).then(() => {
+      callback();
+    }));
   }
 
   getMoves(producer: (moves: Array<MoveDto>) => void): void {
@@ -30,6 +33,7 @@ export class ApiclientService {
       }).then((response: any) => {
         var range: any = response.result;
         if (range.values.length > 0) {
+          this.keys = range.values[0];
           for (let i = 1; i < range.values.length; i++) {
             var row = range.values[i];
             if (row[0]) {
@@ -48,17 +52,22 @@ export class ApiclientService {
 
   appendData(moveDto: MoveDto) {
     this.initClient(() => {
-      const values = Object.values(moveDto);
-      console.log(values);
-      const sheetRange = 'Tanzfiguren!A500:S500'
-      gapi.client.sheets.spreadsheets.values.append({
-        spreadsheetId: this.settingsService.secret?.sheetId as string,
-        range: sheetRange,
-        resource: { values: values }
-      }).then((response: any) => {
-        console.log(response);
-      }, (response: any) => {
-        console.log('Error: ' + response.result.error.message);
+      gapi.auth.authorize({
+        response_type: 'permission',
+        scope: 'https://www.googleapis.com/auth/spreadsheets',
+        client_id: ""
+      }, () => {
+        const sheetRange = 'Tanzfiguren!A500:S500'
+        gapi.client.sheets.spreadsheets.values.append({
+          spreadsheetId: this.settingsService.secret?.sheetId as string,
+          range: sheetRange,
+          valueInputOption: "USER_ENTERED",
+          resource: { values: [this.moveToLine(moveDto)] }
+        }).then((response: any) => {
+          console.log(response);
+        }, (response: any) => {
+          console.log('Error: ' + response.result.error.message);
+        });
       });
     });
   }
@@ -85,5 +94,14 @@ export class ApiclientService {
       toDo: row[17],
       links: row[18]
     };
+  }
+
+  private moveToLine(moveDto: MoveDto): string[] {
+    return [moveDto.name, moveDto.dance, toGermanDate(moveDto.date),
+    moveDto.order, moveDto.count, String(moveDto.nameVerified),
+    moveDto.type, moveDto.relatedMoves?.join(", "), moveDto.videoname,
+    moveDto.description, moveDto.sequence, moveDto.sequenceLeader,
+    moveDto.sequenceFollower, moveDto.mind, moveDto.variations,
+    toGermanDate(moveDto.date1), toGermanDate(moveDto.date2), moveDto.toDo, moveDto.links]
   }
 }
--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -1,5 +1,5 @@
 export const regexGermanDate = /(\d\d).(\d\d).(\d\d\d\d)/y;
-
+export const regexIsoDate = /(\d\d\d\d)-(\d\d)-(\d\d)/y;
 
 export const parseDate = (dateString: string): Date => {
     const match = regexGermanDate.exec(dateString);
@@ -9,6 +9,20 @@ export const parseDate = (dateString: string): Date => {
     return new Date(dateString);
 }
 
-export const parseBoolean = (boolString: string) : boolean => {
-    return Boolean(JSON.parse(boolString.toLowerCase()));
+export const toGermanDate = (date: Date): string => {
+    console.log(date);
+    if (!date || isNaN(date.getTime())) {
+        return "";
+    }
+    const isoString = date.toISOString();
+    const match = regexIsoDate.exec(isoString);
+    if (match) {
+        return `${match[3]}.${match[2]}.${match[1]}`
+    }
+    return isoString;
+}
+
+export const parseBoolean = (boolString: string): boolean => {
+    boolString = boolString.toLowerCase();
+    return boolString == "true" || boolString == "wahr";
 }
