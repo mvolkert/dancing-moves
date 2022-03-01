02341a336956519b0c4581fc176f07615d026e77
movolker <marco.volkert@astrum-it.de>
2022-03-01 23:29:53 +0100

    improve easter egg

--- src/app/move-page/move-page.component.ts
+++ src/app/move-page/move-page.component.ts
@@ -57,7 +57,7 @@ export class MovePageComponent implements OnInit {
     relatedMovesOtherDances: [] as string[],
     courseDates: [] as CourseDateDto[],
     videos: [] as VideoDto[],
-    description: "# Ablauf \n## Leader\n- 1 links vor\n- 2 rechts vor\n- 3 links zurück\n- 4 rechts zurück\n- 5 links Gewichtsverlagerung\n- 6 rechts Gewichtsverlagerung\n- 7 links Gewichtsverlagerung\n- 8 rechts Gewichtsverlagerung\n- 9 Bauch raus\n- 10 Arsch raus\n\n## Follower\ngespiegelt\n# Bemerkung\nSchalted extra Power beim Follower frei"
+    description: "# Ablauf \n## Leader\n- 1 links vor\n- 2 rechts vor\n- 3 links zurück\n- 4 rechts zurück\n- 5 links Gewichtsverlagerung\n- 6 rechts Gewichtsverlagerung\n- 7 links Gewichtsverlagerung\n- 8 rechts Gewichtsverlagerung\n- 9 Bauch raus\n- 10 Anker\n\n## Follower\ngespiegelt\n# Bemerkung\nSchalted extra Power beim Follower frei"
   } as MoveDto;
 
   constructor(private route: ActivatedRoute, private dataManager: DataManagerService,
