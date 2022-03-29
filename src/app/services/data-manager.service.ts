f910e4db8b82612ece5784ab7a24f80a2d2ad47f
marco.volkert24@gmail.com <marco.volkert24@gmail.com>
2022-03-30 00:08:08 +0200

    convert embed in links

--- src/app/services/data-manager.service.ts
+++ src/app/services/data-manager.service.ts
@@ -16,7 +16,7 @@ import { RelationType } from '../model/relation-type-enum';
 import { SearchDto } from '../model/search-dto';
 import { SpecialRight } from '../model/special-right';
 import { VideoDto } from '../model/video-dto';
-import { deepCopy, delay, generateSortFn, getRow, olderThanADay } from '../util/util';
+import { deepCopy, delay, generateSortFn, getRow, olderThanADay, convertToEmbed} from '../util/util';
 import { ApiclientService } from './apiclient.service';
 import { NavService } from './nav.service';
 import { SettingsService } from './settings.service';
@@ -88,7 +88,7 @@ export class DataManagerService {
           // deep copy for different options in each move
           move.videos = deepCopy(results.videos.filter(v => videoNameDtos.map(n => n.name).includes(v.name)));
           move.videos.forEach(videoDto => videoDto.link = videoDto.link + videoNameDtos.find(n => n.name === videoDto.name)?.options ?? '');
-          videoNameDtos.filter(v => v.name.startsWith("http")).map(v => { return { name: v.name, link: v.name } as VideoDto }).forEach(v => move.videos.push(v));
+          videoNameDtos.filter(v => v.name.startsWith("http")).map(v => { return { name: v.name, link: convertToEmbed(v.name)} as VideoDto }).forEach(v => move.videos.push(v));
         }
       }
       this.setMoves(results.moves);
